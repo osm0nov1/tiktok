d29626b894f477e1d52f80aad50a630ee405a12c
osm0nov1 <osmonovrashit@gmail.com>
2022-02-13 17:08:13 +0600

    Initial Commit

---
+++ tiktok/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tiktok
+//
+//  Created by Rashit Osmonov on 13/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
